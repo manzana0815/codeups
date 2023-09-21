cbd29d837a49dab1f8de4842aa5787ba5e205870
Kurumi <manzana0815@gmail.com>
2023-09-21 11:49:08 +0900

    left only swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,94 @@
 
+var mvSwiper = new Swiper(".js-mv-swiper", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  allowTouchMove: false, // マウスでのスワイプを禁止
+  centeredSlides: true,
+  effect: "fade",
+  loop: true,
+  speed: 2000, // ２秒かけながら次の画像へ移動
+});
+
+
+var Swiper = new Swiper(".js-campaign-swiper", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 24,
+  speed: 2000,
+});
+
+//要素の取得とスピードの設定
+var box = $('.js-color-box'),
+  speed = 700;
+
+//.js-color-boxの付いた全ての要素に対して下記の処理を行う
+box.each(function () {
+  $(this).append('<div class="color"></div>')
+  var color = $(this).find($('.color')),
+    image = $(this).find('img');
+  var counter = 0;
+
+  image.css('opacity', '0');
+  color.css('width', '0%');
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on('inview', function () {
+    if (counter == 0) {
+      $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+        image.css('opacity', '1');
+        $(this).css({ 'left': '0', 'right': 'auto' });
+        $(this).animate({ 'width': '0%' }, speed);
+      })
+      counter = 1;
+    }
+  });
+});
+
+
+document.querySelector('.to-top').addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  $(".js-hamburger").on("click", function () {
+    $(".header").toggleClass("is-open")
+    $(".hamburger").toggleClass("is-open");
+    $(".header__drawer").toggleClass("is-open");
 
+    if ($(".header__drawer").hasClass("is-open")) {
+      $("body").css("overflow", "hidden");
+    } else {
+      $("body").css("overflow", "visible");
+    };
+  });
 
+  $(window).resize(function () {
+    if ($(window).width() >= 768) {
+      $(".header").removeClass("is-open");
+      $(".hamburger").removeClass("is-open");
+      $(".header__drawer").removeClass("is-open");
+      $("body").css("overflow", "visible");
+    }
+  });
+  var topBtn = $('.js-to-top');
+  topBtn.hide();
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      //ボタンの表示方法
+      topBtn.fadeIn();
+    } else {
+      //ボタンの非表示方法
+      topBtn.fadeOut();
+    }
+  });
 });
